094e27acb6315daa7352464d1cad4ba1abba09bf
kristin.e <k.erken@westminster.ac.uk>
2024-11-19 14:30:33 +0000

    First commit

---
+++ pages/_app.js
@@ -0,0 +1,37 @@
+import Preloader from "@/src/components/Preloader";
+import "@/styles/globals.css";
+import { Fragment, useEffect, useState } from "react";
+import Head from 'next/head'
+
+export default function App({ Component, pageProps }) {
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 1500);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="author" content="themepaa" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta name="keywords" content="Kristin Erken - Portfolio" />
+        <meta name="description" content="Kristin Erken - Artist" />
+        <title>Kristin Erken - Artist</title>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+        <link href="assets/css/style.css" rel="stylesheet" />
+      </Head>
+      <Fragment>
+      {loader && <Preloader />}
+      <Component {...pageProps} />
+    </Fragment>
+    </>
+
+  );
+}
