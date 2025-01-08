4c51a38369bf93000a26146a37ba03806e549d6a
kristin.e <k.erken@westminster.ac.uk>
2025-01-08 17:27:18 +0000

    feat: Optimised images, add desertscape and dreaming paintings

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Contact from "@/src/components/Contact";
 import Footer from "@/src/components/Footer";
 import Header from "@/src/components/Header";
-import Home from "@/src/components/Home";
+// import Home from "@/src/components/Home";
 import Portfolio from "@/src/components/Portfolio";
-import Services from "@/src/components/Services";
+// import Services from "@/src/components/Services";
 import Skill from "@/src/components/Skill";
-import Testimonial from "@/src/components/Testimonial";
+// import Testimonial from "@/src/components/Testimonial";
 import ImageView from "@/src/components/popup/ImageView";
-import VideoView from "@/src/components/popup/VideoView";
+// import VideoView from "@/src/components/popup/VideoView";
 import { boston } from "@/src/utils";
 import { Fragment, useEffect } from "react";
 const Index = () => {
@@ -19,29 +19,15 @@ const Index = () => {
   return (
     <Fragment>
       <ImageView />
-      <VideoView />
+      {/* <VideoView /> */}
       {/* End */}
       {/* Header */}
       <Header />
       {/* End Header */}
       {/* Main */}
       <main className="wrapper">
-        {/* Home Section */}
-        {/* <Home /> */}
-        {/* End Home Section */}
-        {/* Services Section */}
-        {/* <Services /> */}
-        {/* End Services Section */}
-         {/* Work Section */}
-         <Portfolio />
-        {/* End Work Section */}
-        {/* Skill Section */}
+        <Portfolio />
         <Skill />
-        {/* End Skill Section */}
-        {/* testimonial Section */}
-        {/* <Testimonial /> */}
-        {/* End testimonial Section */}
-        {/* Contact Section */}
         <Contact />
         {/* End Contact Section */}
         {/* Effect */}
