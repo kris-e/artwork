361e0231fbcfbdfe1f7d44032b35ce656bb0e134
kristin.e <k.erken@westminster.ac.uk>
2025-01-08 14:29:37 +0000

    chore: New port 4000 for dev env plus delete Video.js

--- src/components/Video.js
+++
@@ -1,6 +0,0 @@
-import { YouTubeEmbed } from '@next/third-parties/google'
-
-export default function VideoComponent({ videoId }) {
-
-  return <YouTubeEmbed videoid={videoId} params="controls=1?rel=0" />
-}
